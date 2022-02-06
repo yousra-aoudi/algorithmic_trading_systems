dd543efe4746d607b2f93e142d36412ca42d0ab2
yousra aoudi <97890475+yousra-aoudi@users.noreply.github.com>
2022-02-06 20:32:18 +0000

    Create SimulatedRealClock.py

---
+++ trading_system/SimulatedRealClock.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+
+class SimulatedRealClock:
+	def __init__(self,simulated=False):
+		self.simulated = simulated
+		self.simulated_time = None
+		
+	def process_order(self,order):
+		self.simulated_time= datetime.strptime(order['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+		
+	def getTime(self):
+		if not self.simulated:
+			return datetime.now()
+    else:
+       return self.simulated_time
+
+realtime=SimulatedRealClock()
+print(realtime.getTime())
+# It will return the date/time when you run this code
+simulatedtime=SimulatedRealClock(simulated=True)
+simulatedtime.process_order({'id' : 1, 'timestamp' : '2018-06-29 08:15:27.243860'})
