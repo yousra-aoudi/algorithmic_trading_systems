0680cd171d07fddd0492705c0b900ae47bc932d9
yousra aoudi <97890475+yousra-aoudi@users.noreply.github.com>
2022-02-06 20:58:33 +0000

    Update SimulatedRealClock.py

--- trading_system/SimulatedRealClock.py
+++ trading_system/SimulatedRealClock.py
@@ -22,3 +22,34 @@ simulatedtime=SimulatedRealClock(simulated=True)
 simulatedtime.process_order({'id' : 1, 'timestamp' : '2018-06-29 08:15:27.243860'})
 print(simulatedtime.getTime())
 # It will return 2018-06-29 08:15:27.243860
+
+class TimeOut(threading.Thread):
+  def __init__(self,sim_real_clock,time_to_stop,fun):
+		super().__init__()
+		self.time_to_stop=time_to_stop
+		self.sim_real_clock=sim_real_clock
+		self.callback=fun
+		self.disabled=False
+	
+	def run(self):
+		while not self.disabled and self.sim_real_clock.getTime() < self.time_to_stop:
+			sleep(1)
+			if not self.disabled:
+				self.callback()
+				
+class OMS:
+	def __init__(self,sim_real_clock):
+		self.sim_real_clock = sim_real_clock
+		self.five_sec_order_time_out_management= TimeOut(sim_real_clock, sim_real_clock.getTime()+timedelta(0,5), self.onTimeOut)
+		
+	def send_order(self):
+		self.five_sec_order_time_out_management.disabled = False
+		self.five_sec_order_time_out_management.start()
+		print('send order')
+	
+	def receive_market_reponse(self):
+		self.five_sec_order_time_out_management.disabled = True
+	
+	def onTimeOut(self):
+		print('Order Timeout Please Take Action')
+  
